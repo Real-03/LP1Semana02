55a6da633a24d92b14063842b33df6d8d0339058
Tomás Real <tomasreal.art3d@gmail.com>
2025-02-27 15:57:25 +0000

    Create project and Ask for input

---
+++ Cilindro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            double height = 0.0, raio = 0.0;
+            Console.Write("Introduz a altura do cilindro: ");
+            height=double.Parse(Console.ReadLine());
+            Console.Write("Introduz o Raio do cilindro: ");
+            raio=double.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
