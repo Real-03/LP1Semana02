fd6142830411cc8d289a9a6ca1deb0ac5861bc60
Tomás Real <tomasreal.art3d@gmail.com>
2025-02-27 14:11:10 +0000

    Add SMoothOperator Project

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int number = 0;
+            Console.Write("Insere número inteiro: ");
+            number =Int32.Parse(Console.ReadLine());
+            Console.WriteLine(number--);
+            Console.WriteLine(number++);
+        }
+    }
+}
