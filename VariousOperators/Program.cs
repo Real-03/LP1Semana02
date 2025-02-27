0c122a2fe3244a8522622a0762d8adab328698a4
Tomás Real <tomasreal.art3d@gmail.com>
2025-02-27 14:11:55 +0000

    Add project VariousOperators

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            byte number = 0;
+            Console.Write("Insere número inteiro-não negativo: ");
+            number =byte.Parse(Console.ReadLine());
+            Console.WriteLine(number/2);
+            Console.WriteLine(number<<3);
+            Console.WriteLine(number^6);
+            Console.WriteLine(number>10);
+        }
+    }
+}
