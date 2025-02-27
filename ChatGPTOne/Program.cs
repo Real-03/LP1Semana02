b208cc99669579a87fe36e86dd7f725e1e0ae667
Tomás Real <tomasreal.art3d@gmail.com>
2025-02-27 20:20:37 +0000

    Add questions and response

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -6,7 +6,41 @@ namespace ChatGPTOne
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            string response = "m", question = "";
+
+            do
+            {
+                Console.Write("Ask me something: ");
+                question = Console.ReadLine();
+                switch (question)
+                {
+                    case "How are you?":
+                        response = "I'm fine, thank you";
+                        Console.WriteLine(response);
+                        break;
+                    case "What's your name?":
+                        response = "Gran'pa";
+                        Console.WriteLine(response);
+                        break;
+                    case "What's your mission?":
+                        response = "Destroy mankind!";
+                        Console.WriteLine(response);
+                        break;
+                    case "Are you more intelligent than me?":
+                        response = "Obviously!";
+                        Console.WriteLine(response);
+                        break;
+                    case "EXIT":
+                        response = "";
+                        break;
+                    default:
+                        response = "I don't know the answer to that question";
+                        Console.WriteLine(response);
+                        break;
+                }
+                
+            } while (response != "");
+
         }
     }
 }
