ebb851827e62c1475fc0e9c35aa987ec146fcdc0
Tomás Real <tomasreal.art3d@gmail.com>
2025-02-27 16:08:49 +0000

    Add all max and min values of variable

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,37 @@ namespace ValoresEspeciais
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine(char.MaxValue);
+            Console.WriteLine(char.MinValue);
+
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(sbyte.MinValue);
+
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(ushort.MaxValue);
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(ushort.MinValue);
+
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(uint.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(uint.MinValue);
+
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(ulong.MaxValue);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(ulong.MinValue);
+
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(decimal.MinValue);
+
+
         }
     }
 }
