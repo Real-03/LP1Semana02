8342aae5698cd6d2bf71fd41e4e79911c33f9f57
Real-03 <tomasreal.art3d@gmail.com>
2025-02-28 14:56:35 +0000

    Add files via upload

---
+++ PerfectCountdown/Program.cs
@@ -0,0 +1,43 @@
+﻿﻿using System;
+
+namespace PerfectCountdown
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            int start=0, step = 0;
+            bool response = false;
+            do{
+                Console.Write("Start Number:");
+                start = Int32.Parse(Console.ReadLine());
+                Console.Write("Step Number:");
+                step = Int32.Parse(Console.ReadLine());
+
+                if(start <1 || step <1)
+                {
+                    Console.WriteLine("Out-of-range start or step: Try again");
+                }
+                else if(start <= step)
+                {
+                    Console.WriteLine("Start must be higher then step: Try again");
+                }
+                else if(start%step!=0)
+                {
+                    Console.WriteLine("Start not divisible by step: Try again");
+                }
+                else
+                {
+                    response = true;
+                }
+
+
+            }while(response == false);
+            for(int i = start; i>=0; i-=step)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
